236fee33d923a0f23b494b8afe32e6f6e2c8569e
Kenneth Quirós <kenquiros64@gmail.com>
2018-09-03 02:08:38 -0600

    Adding test for get all songs

---
+++ main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io/ioutil"
+	"music-data-deck/controllers"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/zenazn/goji/web"
+)
+
+// ParseResponse parse response message
+func ParseResponse(res *http.Response) (string, int) {
+	defer res.Body.Close()
+	contents, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(contents), res.StatusCode
+}
+
+// TestAllSongs Test songs
+func TestAllSongs(t *testing.T) {
+	m := web.New()
+	m.Get("/songs", controllers.AllSongs)
+	ts := httptest.NewServer(m)
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/songs")
+	if err != nil {
+		t.Error("unexpected")
+	}
+	_, s := ParseResponse(res)
+	if s != http.StatusOK {
+		t.Error("Invalid status code")
+	}
+}
